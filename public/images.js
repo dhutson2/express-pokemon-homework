19165473b5a4672813bf2ed465178a820e0435f3
Derek Hutson <denverd1301@gmail.com>
2019-07-17 16:09:27 -0600

    Index.ejs rendered with images

---
+++ public/images.js
@@ -0,0 +1,25 @@
+const images = [ 
+    {
+        img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/venusaur.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/charmander.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/charizard.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/squirtle.jpg"
+    },
+    {
+        img: "http://img.pokemondb.net/artwork/wartortle.jpg"
+    }
+];
+
+module.exports = images;
--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express();
+app.use(express.static('public'))
 
 const Pokemon = require('./models/pokemon')
+const Images = require('./public/images')
 
 app.get('/pokemon', (req,res) => {
-    console.log(Pokemon)
-    res.send(Pokemon)
+    res.render('index.ejs', {
+        pokemon: Pokemon,
+        image: Images
+    })
+})
+
+app.get('/pokemon/:index', (req,res) => {
+
 })
 
 app.listen(3000, () => {
