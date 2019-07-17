4bdde70467d960c48c8a18f0564d907a6611b210
Derek Hutson <denverd1301@gmail.com>
2019-07-17 15:38:55 -0600

    Connected DB, can see JSON in browser

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const Pokemon = require('./models/pokemon')
+
+app.get('/pokemon', (req,res) => {
+    console.log(Pokemon)
+    res.send(Pokemon)
+})
+
 app.listen(3000, () => {
     console.log('Pokemon listening on 3000')
 })
